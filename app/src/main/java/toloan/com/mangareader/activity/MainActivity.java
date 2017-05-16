075e658dc45dba33454e53d686a08660bdba312c
toloan <toloan103@gmail.com>
2017-05-16 18:17:49 +0700

    init_project

---
+++ app/src/main/java/toloan/com/mangareader/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package toloan.com.mangareader.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import toloan.com.mangareader.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
